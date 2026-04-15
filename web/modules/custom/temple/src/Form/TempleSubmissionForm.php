2fb005b46b083eafcd054592f490d0612687443b
SivasuriyanMohan <sivasuriyan.mohan@accenture.com>
2026-04-15 12:24:42 +0530

    Added modules JSON API and Pathauto, created new temples

--- web/modules/custom/temple/src/Form/TempleSubmissionForm.php
+++ web/modules/custom/temple/src/Form/TempleSubmissionForm.php
@@ -5,7 +5,7 @@ namespace Drupal\temple\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\node\Entity\Node;
-use Drupal\taxonomy\Entity\Term;
+use Drupal\file\Entity\File;
  
 class TempleSubmissionForm extends FormBase {
  
@@ -15,45 +15,62 @@ class TempleSubmissionForm extends FormBase {
  
   public function buildForm(array $form, FormStateInterface $form_state) {
  
-    // Temple Name
+    
     $form['title'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Temple Name'),
       '#required' => TRUE,
     ];
  
-    // Description
+  
     $form['description'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Description'),
       '#required' => TRUE,
     ];
  
-    // Location (taxonomy)
-    $form['location'] = [
-      '#type' => 'entity_autocomplete',
-      '#title' => $this->t('Location'),
-      '#target_type' => 'taxonomy_term',
-      '#selection_settings' => [
-        'target_bundles' => ['location'],
-      ],
-      '#required' => TRUE,
-    ];
- 
-    // Spiritual Tradition
-    $form['spiritual_tradition'] = [
-      '#type' => 'entity_select_list',
-      '#title' => $this->t('Spiritual Tradition'),
-      '#target_type' => 'taxonomy_term',
-      '#selection_settings' => [
-        'target_bundles' => ['spiritual_tradition'],
-      ],
-      '#required' => TRUE,
-    ];
- 
 
+    $form['address'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Address'),
+      '#required' => TRUE,
+    ];
  
-    // Submit button
+    // Spiritual Tradition (Select)
+    $form['spiritual_tradition'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Spiritual Tradition'),
+      '#options' => $this->getTaxonomyOptions('spiritual_tradition'),
+      '#required' => TRUE,
+    ];
+ 
+    // Location (Select)
+    $form['location'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Location'),
+      '#options' => $this->getTaxonomyOptions('location'),
+      '#required' => TRUE,
+    ];
+ 
+     // Address
+    $form['popular_for'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Popular For'),
+      '#required' => TRUE,
+    ];
+ 
+    // Image Upload
+    $form['image'] = [
+      '#type' => 'managed_file',
+      '#title' => $this->t('Temple Image'),
+      '#upload_location' => 'public://temple_images/',
+      '#required' => TRUE,
+      '#upload_validators' => [
+        'file_validate_extensions' => ['png jpg jpeg'],
+      ],
+    ];
+ 
+    // Submit
     $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit Temple'),
@@ -66,27 +83,62 @@ class TempleSubmissionForm extends FormBase {
  
     $current_user = \Drupal::currentUser();
  
+    // Handle image
+    $file = File::load($form_state->getValue('image')[0]);
+    $file->setPermanent();
+    $file->save();
+ 
     // Create node
     $node = Node::create([
       'type' => 'temple',
       'title' => $form_state->getValue('title'),
       'uid' => $current_user->id(),
       'status' => 0, // Draft
-      'field_description' => [
+ 
+      'body' => [
         'value' => $form_state->getValue('description'),
         'format' => 'basic_html',
       ],
-      'field_location' => [
-        'target_id' => $form_state->getValue('location'),
+ 
+      'field_address' => [
+        'value' => $form_state->getValue('address'),
       ],
-      'field_spiritual_tradition' => [
+ 
+      'field_spirituality' => [
         'target_id' => $form_state->getValue('spiritual_tradition'),
       ],
+ 
+      'field_templelocation' => [
+        'target_id' => $form_state->getValue('location'),
+      ],
+ 
+      'field_popular' => [
+        'value' => $form_state->getValue('popular_for'),
+      ],
+ 
+      'field_images' => [
+        'target_id' => $file->id(),
+      ],
+ 
+
     ]);
  
     $node->save();
  
     \Drupal::messenger()->addMessage($this->t('Temple submitted successfully and is pending review.'));
+  }
+ 
+  private function getTaxonomyOptions($vocabulary) {
+    $terms = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term')
+      ->loadTree($vocabulary);
+ 
+    $options = [];
+    foreach ($terms as $term) {
+      $options[$term->tid] = $term->name;
+    }
+ 
+    return $options;
   }
  
 }
